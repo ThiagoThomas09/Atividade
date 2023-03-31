376b8a574cea5534054a3262b957403d807e9121
Asahi09 <thiagothomas09@outlook.com>
2023-03-30 21:39:31 -0300

    código

--- src/auladia3003/Atividade.java
+++ src/auladia3003/Atividade.java
@@ -1,10 +1,23 @@
 package auladia3003;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Date;
+
 public class Atividade {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(new Locale("pt", "BR"));
+		
+		Calendar hoje = Calendar.getInstance();
+		int hora = hoje.get(Calendar.HOUR_OF_DAY);
+		int min = hoje.get(Calendar.MINUTE);
+		DateFormat df4 = DateFormat.getDateInstance(DateFormat.FULL);
+		Date data = new Date();
+		System.out.println("A data é: " + df4.format(data) +
+				", o horário é: " + hora + "h " + min + "min");
 	}
 
 }
